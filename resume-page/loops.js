5b4d4f263743c974b0edb33fd95645d2e36b0f72
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-20 21:23:35 -0500

    Commit 9 - Created loops.js file

---
+++ resume-page/loops.js
@@ -0,0 +1,31 @@
+console.log("Ready to Go!");
+
+//for (i=0; i<=99; i++) {
+//  console.log(i);
+//}
+
+//for (i=99; i>=0; i--) {
+//  console.log(i);
+//}
+
+//for (i=0; i<=98; i+=2) {
+//  console.log(i);
+//}
+
+//for (i=98; i>=0; i-=2) {
+//  console.log(i);
+//}
+
+//for (i=49; i<=72; i++) {
+//  console.log(i);
+//}
+
+//for (i=81; i>=26; i--) {
+//  console.log(i);
+//}
+
+for (i=3; i<=90; i++) {
+  if (i % 3 == 0) {
+    console.log(i);
+}
+}
