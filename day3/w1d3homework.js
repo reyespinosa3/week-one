7e818ce5b07c86698f8088f64ca3562882e25547
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-21 20:23:11 -0500

    Commit 3 - Excited Kittens answered

--- day3/w1d3homework.js
+++ day3/w1d3homework.js
@@ -9,3 +9,24 @@ for (let i=0; i<=20; i++) {
 for (let i=0; i<=200; i+=2) {
     console.log(i);
 }
+
+//EXCITED KITTEN
+var a = "...human...why you taking pictures of me?...";
+var b = "...the catnip made me do it...";
+var c = "...why does the red dot always get away...";
+
+for (i=0; i<=20; i++) {
+  if (i%2!==0) {
+    var select = Math.random() * 10;
+      if (select <= 3) {
+        console.log(a);
+      } else if (select >= 6) {
+        console.log(b);
+      } else if (select > 3 || select < 6) {
+        console.log(c)
+      }
+  } else {
+    console.log("Love me, pet me! HSSSSS!");
+  }
+
+}
